f9f12b29fade5e7d88dd1c6a0d1b9fb8a35604cf
mrpedrobraga <mrhenribraga@gmail.com>
2026-04-05 18:13:50 -0300

    feat: Added cool example.

--- examples/build/main.rs
+++ examples/build/main.rs
@@ -1,14 +1,14 @@
 use pretty::{Doc, RcDoc};
 use sol_lang::parser::{
     self,
-    ast::{Scene, Script},
+    ast::{Module, Scene},
 };
 
 fn main() {
     let input = std::fs::read_to_string("./examples/dist/main.sol").expect("File to be found");
     let (_, script) = parser::nom::p_script(&input).expect("Parsing .sol correctly!");
 
-    let output = q_script(&script);
+    let output = q_module(&script);
     let mut w = Vec::new();
     output
         .render(80, &mut w)
@@ -17,11 +17,11 @@ fn main() {
     std::fs::write("./examples/dist/main2.sol", formatted).unwrap();
 }
 
-fn q_script(script: &Script) -> RcDoc {
-    RcDoc::intersperse(script.scenes.iter().map(q_scene), Doc::line())
+fn q_module(module: &'_ Module) -> RcDoc<'_> {
+    RcDoc::intersperse(module.scenes.iter().map(q_scene), Doc::line())
 }
 
-fn q_scene(scene: &Scene) -> RcDoc {
+fn q_scene(scene: &'_ Scene) -> RcDoc<'_> {
     RcDoc::text("scene ")
         .append(scene.name.clone())
         .append("\n")
